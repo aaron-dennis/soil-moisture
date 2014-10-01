e5087ac01fe13e175ac3e60a8b599e912bd71b2a
aaron-dennis <aaron.p.dennis@gmail.com>
2014-10-01 12:31:17 -0400

    for loop still returns errors

--- data-from-dbf-to-csv.R
+++ data-from-dbf-to-csv.R
@@ -15,5 +15,49 @@ rm(list = ls())
 #install.packages("foreign")
 library(foreign)
 
+# Read ecoregions ID and name data
+ecoregions.data <- read.csv("~/soil-moisture/ecoregions-data/ecoregions.csv", row.names = "US_L3NAME")
 
-data <- read.dbf("/Users/aarondennis/soil-moisture/ecoregions-data/level3/SM_20060605_mean/SM_Mean.dbf")
+ecoregions.mean <- ecoregions.data
+ecoregions.sd  <- ecoregions.data
+
+# Identify working directory
+wd <- "~/soil-moisture/ecoregions-data/level3/"
+
+# Read data folders for DBF file metadata
+data.folders <- list.files(wd)
+
+# Define function to extract date from folder name
+getDate <- function(folder.name) {
+  as.Date(substr(folder.name, 4, 11), "%Y%m%d")
+}
+
+# Define function to extract type of statistic (either mean or standard deviation)
+getStat <- function(folder.name) {
+  substr(folder.name, 13, nchar(folder.name))
+}
+
+# Define function to read file DBF and return as dataframe
+getData <- function(folder.name) {
+  data <- read.dbf(paste("~/soil-moisture/ecoregions-data/level3/", folder.name, "/SM_", statistic, ".dbf", sep = ""))
+  row.names(data) <- data$Ecoregion
+  data <- data[2]
+}
+
+# Loop through folders and append data to ecoregions data
+for (i in 1:length(data.folders)) {
+  statistic <- getStat(data.folders[i])
+  date <- getDate(data.folders[i])
+  data <- getData(data.folders[i])
+  colnames(data) <- date
+  if (statistic == "mean") {
+    ecoregions.mean <- merge(ecoregions.mean, data, by = "row.names")
+  }
+  else if (statistic == "sd") {
+    ecoregions.sd <- merge(ecoregions.sd, data, by = "row.names")
+  }
+  else {
+    print(paste("error with", statistic, date))
+  }
+}
+?merge
